63e1110095c236d597d3a82ed03264bcc885bbb1
klMse <git@dnvrqq.com>
2025-03-02 00:46:41 +0100

    Implement the bulk of the kernel module

--- kernel-module/bloenk.c
+++ kernel-module/bloenk.c
@@ -1,149 +1,197 @@
-// #include <linux/init.h>
-// #include <linux/kernel.h>
-// #include <linux/module.h>
+#include <linux/led-class-multicolor.h>
+#include <linux/leds.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/property.h>
+#include <linux/slab.h>
+#include <linux/usb.h>
 
-// static int __init bloenk_init(void)
-// {
-// 	return 0;
-// }
+#define VENDOR_ID  0x16c0
+#define PRODUCT_ID 0x05dc
 
-// static void __exit bloenk_exit(void)
-// {
+#define BLOENK_RQ_SET_CURRENT_LED  0
+#define BLOENK_RQ_SET_COLOR_R      1
+#define BLOENK_RQ_SET_COLOR_G      2
+#define BLOENK_RQ_SET_COLOR_B      3
+#define BLOENK_RQ_WRITE_TO_LEDS    4
+#define BLOENK_RQ_GET_LEDCOUNT     6
 
-// }
+#define BLOENK_LED_SUBLEDS 3
 
-// module_init(bloenk_init);
-// module_exit(bloenk_exit);
+struct bloenk_led {
+	struct led_classdev_mc mc_cdev;
+	struct bloenk_device *bloenk_dev;
+	unsigned int intensities[BLOENK_LED_SUBLEDS];
+	u8 index;
+};
+#define get_bloenk_led(d) container_of(container_of(d, struct led_classdev_mc, led_cdev), struct bloenk_led, mc_cdev)
 
-// MODULE_DESCRIPTION("bloenk USB driver");
-// MODULE_AUTHOR("klMse");
-// MODULE_LICENSE("GPL");
+struct bloenk_device {
+	struct usb_device *usb_dev;
+	struct bloenk_led *bleds;
+	u8 led_count;
+	u8 current_led;
+	struct mutex io_mutex;
+};
 
-/*
- * usbled.c - A Linux kernel driver to control a custom USB LED device
- * via /sys/class/leds.
- *
- * This driver registers three LED class devices (red, green, blue).
- * Each brightness change sends a USB vendor request to:
- *   - Set the current LED (index 0),
- *   - Set the corresponding channel value, and then
- *   - Write the LED update to the device.
- *
- * The vendor-specific request codes must match those in the firmware.
- */
+static struct mc_subled channels[BLOENK_LED_SUBLEDS] = {
+	{
+		.color_index = LED_COLOR_ID_RED,
+		.channel = BLOENK_RQ_SET_COLOR_R,
+	},
+	{
+		.color_index = LED_COLOR_ID_GREEN,
+		.channel = BLOENK_RQ_SET_COLOR_G,
+	},
+	{
+		.color_index = LED_COLOR_ID_BLUE,
+		.channel = BLOENK_RQ_SET_COLOR_B,
+	},
+};
 
- #include <linux/module.h>
- #include <linux/usb.h>
- #include <linux/leds.h>
- #include <linux/mutex.h>
- #include <linux/slab.h>
- 
- #define VENDOR_ID  0x16c0  // Replace with your device's vendor ID
- #define PRODUCT_ID 0x05dc  // Replace with your device's product ID
- 
- #define CUSTOM_RQ_SET_CURRENT_LED  0
- #define CUSTOM_RQ_SET_COLOR_R      1
- #define CUSTOM_RQ_SET_COLOR_G      2
- #define CUSTOM_RQ_SET_COLOR_B      3
- #define CUSTOM_RQ_WRITE_TO_LEDS    4
- #define CUSTOM_RQ_GET_LEDCOUNT     6
- 
- static struct usb_device *usb_led_dev;
- static struct led_classdev **usb_leds;
- static struct mutex usb_led_mutex;
- static int led_count = 0;
- 
- static int usb_led_control(struct usb_device *dev, u8 request, u8 value, u8 index)
- {
-	 return usb_control_msg(dev, usb_sndctrlpipe(dev, 0), request,
-							USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_DIR_OUT,
-							value, index, NULL, 0, USB_CTRL_SET_TIMEOUT);
- }
- 
- static int usb_led_get_count(struct usb_device *dev)
- {
-	return 4;
- }
- 
- static void usb_led_set_brightness(struct led_classdev *led_cdev,
-									enum led_brightness brightness)
- {
-	 int index = (int)(uintptr_t)led_cdev->dev->platform_data;
-	 mutex_lock(&usb_led_mutex);
-	 usb_led_control(usb_led_dev, CUSTOM_RQ_SET_CURRENT_LED, index, 0);
-	 usb_led_control(usb_led_dev, CUSTOM_RQ_SET_COLOR_R, brightness, 0);
-	 usb_led_control(usb_led_dev, CUSTOM_RQ_WRITE_TO_LEDS, 0, 0);
-	 mutex_unlock(&usb_led_mutex);
- }
- 
- static int usb_led_probe(struct usb_interface *interface, const struct usb_device_id *id)
- {
-	 int i, ret;
-	 usb_led_dev = usb_get_dev(interface_to_usbdev(interface));
-	 mutex_init(&usb_led_mutex);
-	 
-	 led_count = usb_led_get_count(usb_led_dev);
-	 if (led_count <= 0) {
-		 dev_err(&interface->dev, "Failed to get LED count\n");
-		 return -ENODEV;
-	 }
-	 
-	 usb_leds = kcalloc(led_count, sizeof(struct led_classdev *), GFP_KERNEL);
-	 if (!usb_leds)
-		 return -ENOMEM;
-	 
-	 for (i = 0; i < led_count; i++) {
-		 usb_leds[i] = kzalloc(sizeof(struct led_classdev), GFP_KERNEL);
-		 if (!usb_leds[i]) {
-			 ret = -ENOMEM;
-			 goto error;
-		 }
-		 usb_leds[i]->name = kasprintf(GFP_KERNEL, "usb_led_%d", i);
-		 usb_leds[i]->brightness_set = usb_led_set_brightness;
-		 usb_leds[i]->max_brightness = 255;
-		 usb_leds[i]->dev->platform_data = (void *)(uintptr_t)i;
-		 led_classdev_register(&interface->dev, usb_leds[i]);
-	 }
-	 
-	 return 0;
- 
- error:
-	 while (i-- > 0) {
-		 led_classdev_unregister(usb_leds[i]);
-		 kfree(usb_leds[i]->name);
-		 kfree(usb_leds[i]);
-	 }
-	 kfree(usb_leds);
-	 return ret;
- }
- 
- static void usb_led_disconnect(struct usb_interface *interface)
- {
-	 int i;
-	 for (i = 0; i < led_count; i++) {
-		 led_classdev_unregister(usb_leds[i]);
-		 kfree(usb_leds[i]->name);
-		 kfree(usb_leds[i]);
-	 }
-	 kfree(usb_leds);
-	 usb_put_dev(usb_led_dev);
- }
- 
- static struct usb_device_id usb_led_table[] = {
-	 { USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
-	 {}
- };
- MODULE_DEVICE_TABLE(usb, usb_led_table);
- 
- static struct usb_driver usb_led_driver = {
-	 .name = "usb_led",
-	 .id_table = usb_led_table,
-	 .probe = usb_led_probe,
-	 .disconnect = usb_led_disconnect,
- };
- 
- module_usb_driver(usb_led_driver);
- 
- MODULE_LICENSE("GPL");
- MODULE_AUTHOR("Your Name");
- MODULE_DESCRIPTION("USB RGB LED Driver for /sys/class/leds with per-LED control"); 
+static atomic_t counter;
+
+static int bloenk_send_msg(struct usb_device *dev, u8 request, u8 value)
+{
+	return usb_control_msg(dev, usb_sndctrlpipe(dev, 0), request,
+						USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_DIR_OUT,
+						value, 0, NULL, 0, USB_CTRL_SET_TIMEOUT);
+}
+
+static void bloenk_set_brightness(struct led_classdev *cdev, enum led_brightness brightness)
+{
+	struct bloenk_led *bled;
+	struct bloenk_device *bdev;
+	struct mc_subled *subled;
+	bool changed = false;
+	u8 value;
+	int i;
+
+	bled = get_bloenk_led(cdev);
+	bdev = bled->bloenk_dev;
+
+	mutex_lock(&bdev->io_mutex);
+	if (bdev->current_led != bled->index) {
+		bloenk_send_msg(bdev->usb_dev, BLOENK_RQ_SET_CURRENT_LED, bled->index);
+		bdev->current_led = bled->index;
+	}
+
+	for (i = 0; i < bled->mc_cdev.num_colors; ++i) {
+		subled = &bled->mc_cdev.subled_info[i];
+		if (subled->intensity != bled->intensities[i]) {
+			value = brightness * subled->intensity / bled->mc_cdev.led_cdev.max_brightness;
+			bloenk_send_msg(bdev->usb_dev, subled->channel, value);
+			bled->intensities[i] = subled->intensity;
+			changed = true;
+		}
+	}
+
+	if (changed)
+		bloenk_send_msg(bdev->usb_dev, BLOENK_RQ_WRITE_TO_LEDS, 0);
+	mutex_unlock(&bdev->io_mutex);
+}
+
+static int bloenk_probe(struct usb_interface *interface, const struct usb_device_id *id)
+{
+	struct usb_device *usb_dev;
+	struct bloenk_device *bdev;
+	struct bloenk_led *bled;
+	int err = -ENOMEM, retval, i, led_count = 4;
+
+	usb_dev = usb_get_dev(interface_to_usbdev(interface));
+	
+	bdev = kzalloc(sizeof(struct bloenk_device), GFP_KERNEL);
+	if (!bdev)
+		return err;
+
+
+	bdev->bleds = kcalloc(led_count, sizeof(struct bloenk_led), GFP_KERNEL);
+	if (!bdev->bleds)
+		goto err;
+	bdev->led_count = led_count;
+	bdev->usb_dev = usb_dev;
+	mutex_init(&bdev->io_mutex);
+
+	for (i = 0; i < led_count; ++i) {
+		bled = &bdev->bleds[i];
+		bled->mc_cdev.led_cdev.brightness_set = bloenk_set_brightness;
+		bled->mc_cdev.led_cdev.max_brightness = 255;
+		bled->mc_cdev.led_cdev.name = kasprintf(GFP_KERNEL, "bloenk:%d:%d", atomic_read(&counter), i);
+		bled->mc_cdev.led_cdev.color = LED_COLOR_ID_MULTI;
+		if (!bled->mc_cdev.led_cdev.name)
+			goto free_leds;
+
+		bled->mc_cdev.num_colors = BLOENK_LED_SUBLEDS;
+		bled->mc_cdev.subled_info = kzalloc(sizeof(channels), GFP_KERNEL);
+		if (!bled->mc_cdev.subled_info)
+			goto free_name;
+		memcpy(bled->mc_cdev.subled_info, channels, sizeof(channels));
+
+		bled->bloenk_dev = bdev;
+		bled->index = i;
+		retval = led_classdev_multicolor_register(&interface->dev, &bled->mc_cdev);
+		if (retval)
+			goto free_subled;
+
+		continue;
+
+	free_subled:
+		kfree(bled->mc_cdev.subled_info);
+	free_name:
+		kfree(bled->mc_cdev.led_cdev.name);
+		goto free_leds;
+	}
+	atomic_inc(&counter);
+	usb_set_intfdata(interface, bdev);
+
+	return 0;
+
+free_leds:
+	for (; i > 0; --i) {
+		led_classdev_multicolor_unregister(&bdev->bleds[i - 1].mc_cdev);
+		kfree(bdev->bleds[i - 1].mc_cdev.led_cdev.name);
+		kfree(bdev->bleds[i - 1].mc_cdev.subled_info);
+	}
+	kfree(bdev->bleds);
+err:
+	mutex_destroy(&bdev->io_mutex);
+	kfree(bdev);
+	return err;
+}
+
+static void bloenk_disconnect(struct usb_interface *interface)
+{
+	struct bloenk_device *bdev;
+	struct bloenk_led *bled;
+	int i;
+
+	bdev = usb_get_intfdata(interface);
+	for (i = 0; i < bdev->led_count; ++i) {
+		bled = &bdev->bleds[i];
+		led_classdev_multicolor_unregister(&bled->mc_cdev);
+		kfree(bled->mc_cdev.subled_info);
+		kfree(bled->mc_cdev.led_cdev.name);
+	}
+	kfree(bdev->bleds);
+	mutex_destroy(&bdev->io_mutex);
+	kfree(bdev);
+}
+
+static struct usb_device_id bloenk_table[] = {
+	{ USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
+	{}
+};
+MODULE_DEVICE_TABLE(usb, bloenk_table);
+
+static struct usb_driver bloenk_driver = {
+	.name = "bloenk",
+	.id_table = bloenk_table,
+	.probe = bloenk_probe,
+	.disconnect = bloenk_disconnect,
+};
+
+module_usb_driver(bloenk_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("klMse");
+MODULE_DESCRIPTION("USB driver for bloenk");
