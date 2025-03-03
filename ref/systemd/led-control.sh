7085ffea4318eec5baa90151adbe508ea9504458
jabertwo <git@jabertwo.de>
2025-03-03 21:50:06 +0100

    udev rules, systemd service example and manual led changing script

---
+++ ref/systemd/led-control.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <lednum> <brightness> <red> <green> <blue>"
+    exit 1
+fi
+
+# Assign input variables
+LEDNUM=$1
+BRIGHTNESS=$2
+RED=$3
+GREEN=$4
+BLUE=$5
+
+# LED sysfs path (update if different)
+LED_PATH="/sys/class/leds/bloenk:0:$LEDNUM"
+
+echo $LED_PATH
+
+# Set LED brightness
+echo $BRIGHTNESS > $LED_PATH/brightness
+
+# Set individual RGB color intensities
+echo $RED $GREEN $BLUE  > $LED_PATH/multi_intensity
+
---
+++ ref/systemd/warpzone-led-monitor.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Space API URL (bitte anpassen!)
+SPACE_API_URL="https://api.warpzone.ms/spaceapi"
+
+# Liste aller LEDs
+LED_PATHS=(
+    "/sys/class/leds/bloenk:0:0"
+    "/sys/class/leds/bloenk:0:1"
+    "/sys/class/leds/bloenk:0:2"
+    "/sys/class/leds/bloenk:0:3"
+)
+
+while true; do
+    # Warpzone-Status abrufen (1=open, 0=closed)
+    STATUS=$(curl -s "$SPACE_API_URL" | jq -r '.state.open')
+
+    if [[ "$STATUS" == "true" ]]; then
+        for LED in "${LED_PATHS[@]}"; do
+            echo 0 255 0 > "$LED/multi_intensity"
+            echo 255 > "$LED/brightness"
+        done
+    else
+        for LED in "${LED_PATHS[@]}"; do
+	    echo 255 0 0 > "$LED/multi_intensity"
+            echo 255 > "$LED/brightness"
+        done
+    fi
+
+    sleep 30 # Alle 30 Sekunden neu prüfen
+done
